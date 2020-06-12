28147e7eab89ad05d69fd73131758b56e690e8dd
Mike-Cursor <63852043+Mike-Cursor@users.noreply.github.com>
2020-06-12 22:15:38 +0300

    модуляция (часть 1)

---
+++ js/form.js
@@ -0,0 +1,19 @@
+'use strict';
+window.makePicture = function (filterType) {
+  var form = document.querySelector('.img-upload__overlay');
+  var upload = document.querySelector('#upload-file');
+  upload.addEventListener('change', function () {
+    form.classList.remove('hidden');
+  });
+  var cancel = document.querySelector('#upload-cancel');
+  cancel.addEventListener('change', function () {
+    form.classList.add('hidden');
+  });
+  var image = document.querySelector('.img-upload__preview img');
+  var filter = 0;
+  var effectValue = 25;
+  var effectLevel = document.querySelector('.effect-level__pin');
+  effectLevel.addEventListener('mouseup', function () {
+    image.style.filter = filterType[filter] + effectValue + ')';
+  });
+};
