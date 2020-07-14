2c34ebc2ec6f7348ac049a30de82c9edbf75235f
Mike-Cursor <63852043+Mike-Cursor@users.noreply.github.com>
2020-07-14 23:01:40 +0300

    устанение дребезга

--- js/sort.js
+++ js/sort.js
@@ -4,6 +4,7 @@
     var filterBlock = document.querySelector('.img-filters');
     var filterButtons = document.querySelectorAll('.img-filters__button');
     var time = 0;
+    var filter = 0;
     var popularData = data.slice();
     for (var i = popularData.length - 1; i > 0; i--) {
       for (var j = 0; j < i; j++) {
@@ -16,7 +17,7 @@
     }
     filterBlock.classList.remove('img-filters--inactive');
     filterButtons[0].addEventListener('click', function () {
-      if (time < 1) {
+      if (time < 1 && filter !== 0) {
         var images = document.querySelectorAll('.picture');
         for (var l = 0; l < images.length; l++) {
           images[l].remove();
@@ -26,10 +27,11 @@
         filterButtons[1].classList.remove('img-filters__button--active');
         filterButtons[2].classList.remove('img-filters__button--active');
         time = 50;
+        filter = 0;
       }
     });
     filterButtons[1].addEventListener('click', function () {
-      if (time < 1) {
+      if (time < 1 && filter !== 1) {
         var images = document.querySelectorAll('.picture');
         for (var m = 0; m < images.length; m++) {
           images[m].remove();
@@ -50,10 +52,11 @@
         filterButtons[0].classList.remove('img-filters__button--active');
         filterButtons[2].classList.remove('img-filters__button--active');
         time = 50;
+        filter = 1;
       }
     });
     filterButtons[2].addEventListener('click', function () {
-      if (time < 1) {
+      if (time < 1 && filter !== 2) {
         var images = document.querySelectorAll('.picture');
         for (var n = 0; n < images.length; n++) {
           images[n].remove();
@@ -63,6 +66,7 @@
         filterButtons[1].classList.remove('img-filters__button--active');
         filterButtons[0].classList.remove('img-filters__button--active');
         time = 50;
+        filter = 2;
       }
     });
     setInterval(function () {
