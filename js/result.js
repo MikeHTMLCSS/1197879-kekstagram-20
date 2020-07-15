1a0220fda6c2c9698e5c1a88c8aa79ad4a66819f
Mike-Cursor <63852043+Mike-Cursor@users.noreply.github.com>
2020-07-15 10:30:04 +0300

    исправляем закрытие модальных окон

--- js/result.js
+++ js/result.js
@@ -6,42 +6,45 @@
   var errorCancelElement = document.querySelector('.error__button');
   var successBlock = document.querySelector('.success__inner');
   var errorBlock = document.querySelector('.error__inner');
-  var closeSuccessMessageClick = function (evt) {
+  var hover = false;
+  successCancelElement.addEventListener('click', function (evt) {
     if (evt.which === 1) {
       successElement.classList.add('hidden');
-      successCancelElement.removeEventListener('click', closeSuccessMessageClick);
-      successCancelElement.removeEventListener('keydown', closeSuccessMessagekey);
     }
-  };
-  var closeSuccessMessagekey = function (evt) {
+  });
+  errorCancelElement.addEventListener('click', function (evt) {
+    if (evt.which === 1) {
+      errorElement.classList.add('hidden');
+    }
+  });
+  document.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
       successElement.classList.add('hidden');
-      successCancelElement.removeEventListener('click', closeSuccessMessageClick);
-      successCancelElement.removeEventListener('keydown', closeSuccessMessagekey);
+      errorElement.classList.add('hidden');
     }
-  };
+  });
+  successBlock.addEventListener('mouseover', function () {
+    hover = true;
+  });
+  successBlock.addEventListener('mouseout', function () {
+    hover = false;
+  });
+  errorBlock.addEventListener('mouseover', function () {
+    hover = true;
+  });
+  errorBlock.addEventListener('mouseout', function () {
+    hover = false;
+  });
+  document.addEventListener('click', function (evt) {
+    if (evt.which === 1 && !hover) {
+      successElement.classList.add('hidden');
+      errorElement.classList.add('hidden');
+    }
+  });
   window.openSuccessMessage = function () {
     successElement.classList.remove('hidden');
-    successCancelElement.addEventListener('click', closeSuccessMessageClick);
-    document.addEventListener('keydown', closeSuccessMessagekey);
-  };
-  var closeErrorMessageClick = function (evt) {
-    if (evt.which === 1) {
-      errorElement.classList.add('hidden');
-      successCancelElement.removeEventListener('click', closeErrorMessageClick);
-      successCancelElement.removeEventListener('keydown', closeErrorMessagekey);
-    }
-  };
-  var closeErrorMessagekey = function (evt) {
-    if (evt.keyCode === 27) {
-      errorElement.classList.add('hidden');
-      errorCancelElement.removeEventListener('click', closeErrorMessageClick);
-      errorCancelElement.removeEventListener('keydown', closeErrorMessagekey);
-    }
   };
   window.openErrorMessage = function () {
     errorElement.classList.remove('hidden');
-    errorCancelElement.addEventListener('click', closeErrorMessageClick);
-    document.addEventListener('keydown', closeErrorMessagekey);
   };
 })();
